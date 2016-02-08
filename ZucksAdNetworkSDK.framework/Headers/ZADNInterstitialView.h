465becdc5f03b5f68e308144543c04028dbd7c2a
nmorikubo <Naoki_Morikubo@voyagegroup.com>
2016-02-08 15:18:36 +0900

    v4.0.0

---
+++ ZucksAdNetworkSDK.framework/Headers/ZADNInterstitialView.h
@@ -0,0 +1,53 @@
+//
+//  ZADNInterstitialView.h
+//  ZucksAdNetworkSDK
+//
+//  Copyright (C) 2015 Zucks, Inc.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol ZADNInterstitialViewDelegate;
+
+typedef NS_ENUM(NSInteger, ZADNInterstitialLoadErrorType) {
+    ZADNInterstitialLoadErrorTypeOffline = 1,       // オフライン
+    ZADNInterstitialLoadErrorTypeFrameIdUndefined,  // frameIDエラー
+    ZADNInterstitialLoadErrorTypeOtherError         // その他エラー
+};
+
+typedef NS_ENUM(NSInteger, ZADNInterstitialShowErrorType) {
+    ZADNInterstitialShowErrorTypeOffline = 1,     // オフライン
+    ZADNInterstitialShowErrorTypeNoAdData,        // 広告情報未取得
+    ZADNInterstitialShowErrorTypeLoading,         // 広告情報ロード中
+    ZADNInterstitialShowErrorTypeAlreadyShowing,  // 広告表示中
+    ZADNInterstitialShowErrorTypeOtherError       // その他エラー
+};
+
+@interface ZADNInterstitialView : UIView
+
+@property(copy, nonatomic) NSString *frameId;
+@property(weak, nonatomic) id<ZADNInterstitialViewDelegate> delegate;
+
++ (instancetype)sharedInstance;
+- (void)loadAd;
+- (void)show;
+
+- (id)initWithFrame:(CGRect)frame
+    __attribute__((unavailable("initWithFrame is not available")));
+
+@end
+
+@protocol ZADNInterstitialViewDelegate<NSObject>
+@optional
+
+- (void)interstitialViewDidReceiveAd;
+- (void)interstitialViewDidLoadFailAdWithErrorType:
+        (ZADNInterstitialLoadErrorType)errorType;
+- (void)interstitialViewDidShowAd;
+- (void)interstitialViewDidShowFailAdWithErrorType:
+        (ZADNInterstitialShowErrorType)errorType;
+- (void)interstitialViewCancelDisplayRate;
+- (void)interstitialViewDidTapAd;
+- (void)interstitialViewDidDismissAd;
+
+@end
